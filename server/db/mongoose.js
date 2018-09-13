3f6dbcb3b5fa28038fffc55a96a00b91c263f7db
Andrea Babuin <andrea.babuin@tecnicosuperiorekennedy.it>
2018-09-13 22:36:07 +0200

    Add POST /todos and refactor mongoose

---
+++ server/db/mongoose.js
@@ -0,0 +1,10 @@
+let mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/TodoApp');
+
+
+module.exports = {mongoose};
+// module.exports = {
+//     moongose: mongoose
+// };
---
+++ server/server.js
@@ -0,0 +1,47 @@
+let express = require('express');
+let bodyParser = require('body-parser');
+
+let {mongoose} = require('./db/mongoose');
+let {Todo} = require('./models/todo');
+let {User} = require('./models/user');
+
+let port = 3000;
+
+let app = express();
+
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+    let todo = new Todo({
+        text: req.body.text
+    });
+    todo.save().then((doc) =>{
+        res.send(doc);
+    }, (e) => {
+        res.status(400).send(e);
+    });
+});
+
+app.get('/', (req, res) => {
+
+});
+
+app.listen(port, () => {
+    console.log('Started on port ', port);
+});
+
+
+
+//challenge
+// let newTodo2 = new Todo({
+//     text : 'Do the challenge',
+//     completed: true,
+//     completedAt: 01
+// });
+
+// newTodo2.save().then((doc) => {
+//     console.log('newTodo2 was saved!', JSON.stringify(doc, undefined, 2));
+// }, (e) => {
+//     console.log('There was an error: ', e);
+// });
